61855a629c68c289fa958ea0ebac88b65a9dfe6c
xiaxinti <michelle.xia@mail.utoronto.ca>
2018-11-21 22:26:27 -0500

    added mirubot sync related data

---
+++ mirusync/sync_mirudata.php
@@ -0,0 +1,45 @@
+<?php
+include 'miru_common.php';
+include 'sql_param.php';
+$conn = connect_sql($host, $user, $pass, $schema);
+/*lists*/
+$lists = array(
+	'monsterList' => 'MONSTER_NO',
+	'monsterAddInfoList' => 'MONSTER_NO',
+	'evolutionList' => 'TV_SEQ',
+	'skillList' => 'TS_SEQ',
+	'awokenSkillList' => 'TMA_SEQ',
+	'skillLeaderDataList' => 'TS_SEQ'
+);
+foreach($lists as $tablename => $pk){
+$data = json_decode(file_get_contents("https://storage.googleapis.com/mirubot/paddata/padguide/$tablename.json"), true)['items'];
+$fieldnames = default_fieldnames($data[0]);
+// exclude search_data and timestamp
+$fieldnames = array_diff($fieldnames, ['SEARCH_DATA', 'TSTAMP']);
+recreate_table($conn, $data, $tablename, $fieldnames, $pk);
+populate_table($conn, $data, $tablename, $fieldnames);
+}
+/*nickname list*/
+//$gid = array('2070615818', '0');
+/*$fieldnames = array(
+	'nickname',
+	'MONSTER_NO'
+);
+$data = get_google_sheets_data('https://docs.google.com/spreadsheets/d/1EoZJ3w5xsXZ67kmarLE4vfrZSIIIAfj04HXeZVST3eY/pub?gid=0&single=true&output=csv', $fieldnames);
+$tablename = 'nicknames';
+$pk = 'nickname';
+recreate_table($conn, $data, $tablename, $fieldnames, $pk);
+populate_table($conn, $data, $tablename, $fieldnames);
+
+$fieldnames = array(
+	'MONSTER_NO',
+	'basename'
+);
+$data = get_google_sheets_data('https://docs.google.com/spreadsheets/d/1EoZJ3w5xsXZ67kmarLE4vfrZSIIIAfj04HXeZVST3eY/pub?gid=2070615818&single=true&output=csv', $fieldnames);
+$tablename = 'basenames';
+$pk = '';
+recreate_table($conn, $data, $tablename, $fieldnames, $pk);
+populate_table($conn, $data, $tablename, $fieldnames);*/
+
+$conn->close();
+?>
---
+++ rem/add_rates.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Rate Calculator</title>
+	<link rel="stylesheet" type="text/css" href="rate_calc.css">
+	<script src="rate_calc.js"></script>
+</head>
+<body>
+<?php
+
+?>
+</body>
+</html>
