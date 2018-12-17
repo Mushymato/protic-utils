554daa681a996bb2ae7062f47fbc9c7f2d61963a
xiaxinti <michelle.xia@mail.utoronto.ca>
2018-12-17 18:19:55 -0500

    boards moved to Mushymato/pad-boards

--- boards/README.md
+++
@@ -1,6 +0,0 @@
-
-# boards
-
-Stuff for displaying boards
-
-Board solve related functions (FloodFill, solve_board), is ported to php from pad.js in https://github.com/dawnGlare/padsim
--- boards/boards.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA optimal;
-CREATE TABLE boards (
-    bID int PRIMARY KEY AUTO_INCREMENT,
-    size nvarchar(1) NOT NULL DEFAULT 'm', -- s,m,l for 5x4,6x5,7x6
-    pattern nvarchar(42) NOT NULL, -- board pattern as a string
-	combo int NOT NULL, -- number of combos
-	description nvarchar(100)
-);
