12e3a94a9204efd08dd1a2ae1a43e7264a1e677c
xiaxinti <michelle.xia@mail.utoronto.ca>
2019-01-24 19:36:51 -0500

    removed search_id call

--- card_grid.php
+++ card_grid.php
@@ -4,7 +4,12 @@
 <?php
 include 'miru_common.php';
 $id_str = array_key_exists('id', $_GET) ? $_GET['id'] : '4428';
-$id = search_ids($id_str)[0];
+$mon = query_monster(trim($id_str));
+if($mon){
+	$id = $mon['MONSTER_NO'];
+}else{
+	die($id_str . ' not found');
+}
 $card = get_card_grid($id);
  echo '<div>' . $card['html'] . '</div>' . PHP_EOL; ?>
 </body>
