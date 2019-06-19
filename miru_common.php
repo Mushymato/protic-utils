772eb22a93a85a0b4d2dcce934f46e07dae8b113
mushy <wildshinobu@gmail.com>
2019-06-18 21:25:42 -0400

    point to new awks folder

--- miru_common.php
+++ miru_common.php
@@ -455,7 +455,7 @@ $aw = array(
 	16460 => 65,
 	16461 => 66,
 	16462 => 67);
-function awake_icon($id, $w = '31', $h = '32', $awake_url = '/wp-content/uploads/pad-awakenings/', $info_url = 'http://www.puzzledragonx.com/en/awokenskill.asp?s='){
+function awake_icon($id, $w = '31', $h = '32', $awake_url = '/wp-content/uploads/pad-awks/', $info_url = 'http://www.puzzledragonx.com/en/awokenskill.asp?s='){
 	return array('html' => '<a href="' . $info_url . $id . '"><img src="' . $awake_url . $id . '.png" width="' . $w. '" height="' . $h. '"/></a>', 'shortcode' => '[awk id=' . $id . ($w != '31' ? ' w=' . $w . ' h=' . $h : '') . ']');
 }
 function awake_list($awakenings, $w = '31', $h = '32'){
